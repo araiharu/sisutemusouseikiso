983548363627b82e2dbc6baa59092a4265fb9e2f
araiharu <t236131@st.yamagata-u.ac.jp>
2024-10-18 19:31:00 +0900

    Add files via upload

---
+++ cat+bag.c
@@ -0,0 +1,19 @@
+int main()
+{
+    int k;
+    printf("How many cats?");
+    scanf("%d", &k);
+    
+    printf("The number of cats is %d", k);
+    
+    int j;
+    printf("How many bags?");
+    scanf("%d", &j);
+    
+    printf("The number of bags is %d", j);
+    
+    printf("allnumber is? %d", k+j);
+
+
+    return 0;
+}
